72d68a29038e2dc12e779488519d9a7b9b291ca0
jingxiaohu <jingxiaohu@windows10.microdone.cn>
2017-03-21 15:10:22 +0800

    jar to transfer war

--- FileServer/src/main/java/com/jxh/Application.java
+++ FileServer/src/main/java/com/jxh/Application.java
@@ -12,12 +12,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class Application extends SpringServletContainerInitializer{
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
